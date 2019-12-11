4f237a78d30ba826b0ec0eca82a690f4eccbe006
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-11 15:39:22 -0600

    explicitly remove libstdc builds as we are not interested in the old pre C++11 ABI

--- build.py
+++ build.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
                                 archs=["x86_64"],
                                 build_types=["Release"])
     builder.add_common_builds(pure_c=False,shared_option_name="vtk:shared")
+
+    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc")
+
+
     builder.run()
 
-def configure(self):
-    # don't compile for the old ABI
-    del self.settings.compiler.libcxx
