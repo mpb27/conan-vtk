fb8b7d7104f55607ec1bee71ca9a7c24111732df
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-11 10:59:29 -0600

    refine build matrix

--- build.py
+++ build.py
@@ -3,5 +3,9 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="vtk:shared")
+    builder.add_common_builds(shared_option_name="vtk:shared",
+                                pure_c=False,
+                                gcc_versions=["8.0"],
+                                archs=["x86_64"],
+                                build_types=["Release"])
     builder.run()
