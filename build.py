c7a2320a7983f002d57ac7315db60a6ac5c69b6c
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-11 14:32:35 -0600

    parallel build

--- build.py
+++ build.py
@@ -7,3 +7,7 @@ if __name__ == "__main__":
                                 build_types=["Release"])
     builder.add_common_builds(pure_c=False,shared_option_name="vtk:shared")
     builder.run()
+
+def configure(self):
+    # don't compile for the old ABI
+    del self.settings.compiler.libcxx
