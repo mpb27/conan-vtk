3b14e90b61a80c802e2ec9fe85f01a2dbebc73e4
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-11 17:21:19 -0600

    new version of pages

--- build.py
+++ build.py
@@ -4,16 +4,17 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(gcc_versions=["8"],
                                 archs=["x86_64"],
-                                build_types=["Release"])
+                                build_types=["Release"],
+                                curpage="p1", total_pages=2)
     builder.add_common_builds(pure_c=False,shared_option_name="vtk:shared")
 
     builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
 
 
     i=0
-    for settings, options, env_vars, build_requires in builder.items:
+    for settings, options, env_vars, build_requires, reference in builder.items:
         i += 1 
-        named_builds["p%s" % i].append([settings, options, env_vars, build_requires])
+        named_builds["p%s" % i].append([settings, options, env_vars, build_requires, reference])
 
     builder.named_builds = named_builds
 
