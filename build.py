cf2b669acd02842b9218731f8b865419ea5f6315
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-11 12:50:25 -0600

    don't specify minor gcc version

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(gcc_versions=["8.0"],
+    builder = ConanMultiPackager(gcc_versions=["8"],
                                 archs=["x86_64"],
                                 build_types=["Release"])
     builder.add_common_builds(pure_c=False,shared_option_name="vtk:shared")
