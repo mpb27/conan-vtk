cb7e378db522a06a622a0205e3bf0d5c46173a1c
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-11 19:57:23 -0600

    add apple clang

--- build.py
+++ build.py
@@ -3,19 +3,18 @@ from collections import defaultdict
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(gcc_versions=["8"],
+                                apple_clang_versions=["11.0.0"],
                                 archs=["x86_64"],
                                 build_types=["Release"],
-                                curpage="p1", total_pages=2)
+                                curpage="linux", total_pages=2)
     builder.add_common_builds(pure_c=False,shared_option_name="vtk:shared")
 
     builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
 
 
-    i=0
     named_builds = defaultdict(list)
     for settings, options, env_vars, build_requires, reference in builder.items:
-        i += 1 
-        named_builds["p%s" % i].append([settings, options, env_vars, build_requires, reference])
+        named_builds[settings['os']].append([settings, options, env_vars, build_requires, reference])
 
     builder.named_builds = named_builds
 
